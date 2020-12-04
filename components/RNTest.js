58d1d54915ad5734e034f4fcfd0caa7b3fe2b28c
thuan <thuan.hoang@vastbit.com>
2020-12-04 10:47:42 +0700

    first commit

---
+++ components/RNTest.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const RNTest = ({name}) => {
+  return (
+    <View>
+      <Text>{name}</Text>
+    </View>
+  );
+};
+
+export default RNTest;
+
+const styles = StyleSheet.create({});
